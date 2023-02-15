46ef14854e1a75409aa7d736986f925c6bdafca5
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-02-15 20:13:10 +0100

    cleanup css styles

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('postcss-import'),
     require('postcss-nested'),
+    require('postcss-each'),
+    require('postcss-discard-comments'),
+    require('autoprefixer'),
     require('cssnano')
   ]
 }
