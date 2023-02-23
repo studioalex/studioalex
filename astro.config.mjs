74df594858cc1776fced8d47d0bfea172c860371
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-02-23 21:48:38 +0100

    add partytown to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
-import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.studioalex.dev',
-  integrations: [
-    sitemap(),
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
-  ]
-})
+  integrations: [sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), partytown()]
+});
