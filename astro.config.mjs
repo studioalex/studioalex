50f2f92bee2a74abfd4acf0873ad3f87a232ef65
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-02-24 13:30:33 +0100

    update packages and menu icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.studioalex.dev',
   integrations: [sitemap(), tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), partytown()],
-  output: "server",
-  adapter: netlify()
+  }), partytown()]
 });
