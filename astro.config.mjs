5d29ba0b58cabfab78c70abb8cb5f6877d5c3fd3
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-02-24 08:08:50 +0100

    add netlify support to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.studioalex.dev',
   integrations: [sitemap(), tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), partytown()]
+  }), partytown()],
+  output: "server",
+  adapter: netlify()
 });
