54ef72eba7ecbc1b12291ab33e6c2cedda7f77ee
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-02-27 21:05:51 +0100

    set dark mode as default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
   integrations: [sitemap(), tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), partytown()]
-});
+})
