940e5ef587efef630ceac8813ac8848ebe27cc2d
Alexander Sedeke <alexander.sedeke@gmail.com>
2023-03-01 11:55:00 +0100

    add pagination

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -2,11 +2,29 @@ export function formatDate (date) {
   const formatDate = new Date(date).toLocaleDateString('cs-CZ', {
     year: "numeric",
     month: "2-digit",
-    day: "2-digit"
+    day: "2-digit",
+    timeZone: "UTC"
   })
   return formatDate
 }
 
 export function imagePath (name) {
   return `/assets/${name}`
+}
+
+export function formatTipPosts(tips, {
+  sortByDate = true
+} = {}) {
+  // filter for future use
+  const filteredPosts = tips.filter(() => true)
+
+  // sortByDate or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  return filteredPosts
+
 }
